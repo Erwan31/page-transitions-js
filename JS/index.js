62160e7753328263dd242fbfca47e2fe7fdcfc00
Erwan31 <spilmonterwan@gmail.com>
2020-06-08 18:23:37 +0100

    Animation working with gsap and dogstudio highway

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,10 @@
-const run = () => {
-    console.log('Yo I am runinn');
-};
+import Highway from '@dogstudio/highway';
+import Fade from './transition';
+
+
+const H = new Highway.Core({
+    transitions: {
+        default: Fade
+    }
+});
+
